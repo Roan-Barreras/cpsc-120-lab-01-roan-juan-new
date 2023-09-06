0e55ad925899423da273a54b6cad5d14b5aa3925
Barreras <roanmariuse@cs101u-1c.acad.fullerton.edu>
2023-09-06 15:07:10 -0700

    Added a personalized header. My first git commit.

--- part-1/hello.cc
+++ part-1/hello.cc
@@ -1,7 +1,10 @@
-
+//Roan Marius Barreras
+//roanmariuse@csu.fullerton.edu
+//@Roan-Barreras
+//Partner: @barragan-j
 #include <iostream>
 
 int main(int argc, const char *argv[]) {
-  std::cout << "Hello World!\n";
+  std::cout << "Hello Roan!\n";
   return 0;
 }
